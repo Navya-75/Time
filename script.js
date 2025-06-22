1c3fa01bd7b33ce5ce0b69cf7d03c9cc6cd8abff
Navya sri chukka <navyachukka650@gmail.com>
2025-06-22 14:47:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function updateTimes() {
+  const indiaTime = new Date().toLocaleTimeString("en-IN", {
+    timeZone: "Asia/Kolkata",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  });
+
+  const usaTime = new Date().toLocaleTimeString("en-US", {
+    timeZone: "America/New_York",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  });
+
+  document.getElementById("indiaTime").textContent = indiaTime;
+  document.getElementById("usaTime").textContent = usaTime;
+}
+
+setInterval(updateTimes, 1000);
+updateTimes();
